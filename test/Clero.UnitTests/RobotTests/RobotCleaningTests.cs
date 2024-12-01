2fba4a56e1ecf91cc6fb28d1fa14bf6d07dbb283
Antonín Procházka <antonin@postsharp.net>
2024-12-01 15:13:05 +0100

    Cleaning unit test.

---
+++ test/Clero.UnitTests/RobotTests/RobotCleaningTests.cs
@@ -0,0 +1,17 @@
+﻿using Clero.UnitTests.Services;
+
+namespace Clero.UnitTests.RobotTests;
+
+public class RobotCleaningTests
+{
+    [Fact]
+    public void RobotCleansCell()
+    {
+        var room = new CellKind[,] { { CellKind.Dirty } };
+        var robot = new Robot(room, new ThrowingBackOffStrategy(), new(0, 0), Direction.North, 5);
+        robot.Clean().ShouldBe(ActionResult.Success);
+        room[0, 0].ShouldBe(CellKind.Clean);
+        robot.Position.ShouldBe(new(0, 0));
+        robot.BatteryLevel.ShouldBe(0);
+    }
+}
