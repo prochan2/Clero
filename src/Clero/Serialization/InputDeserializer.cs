4cf7bcad546fede624ada94d2c0a9f50a903111d
Antonín Procházka <antonin@postsharp.net>
2024-12-01 17:10:11 +0100

    Deserializer implemented.

---
+++ src/Clero/Serialization/InputDeserializer.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+
+namespace Clero.Serialization;
+
+public class InputDeserializer
+{
+    public (CellKind[,] Room, Position Position, Direction Direction, string[] Commands, int BatteryLevel) Deserialize(Stream stream)
+    {
+        using var document = JsonDocument.Parse(stream);
+        var root = document.RootElement;
+
+        var map = root.GetProperty("map");
+        
+        var rows = new List<List<CellKind>>();
+        List<CellKind>? previousRow = null;
+
+        foreach (var row in map.EnumerateArray())
+        {
+            var rowValues = new List<CellKind>();
+
+            foreach (var cell in row.EnumerateArray())
+            {
+                var cellString = cell.GetString();
+
+                var cellValue = cellString switch
+                {
+                    "S" => CellKind.DirtyUnvisited,
+                    "C" or "null" => CellKind.Obstacle,
+                    _ => throw new InvalidOperationException($"Invalid cell value: {cellString}")
+                };
+                
+                rowValues.Add(cellValue);
+            }
+            
+            if (previousRow != null && rowValues.Count != previousRow.Count)
+            {
+                throw new InvalidOperationException("All rows must have the same number of cells");
+            }
+            
+            previousRow = rowValues;
+            rows.Add(rowValues);
+        }
+        
+        var room = new CellKind[rows[0].Count, rows.Count];
+        
+        for (var y = 0; y < rows.Count; y++)
+        {
+            for (var x = 0; x < rows[y].Count; x++)
+            {
+                room[y, x] = rows[y][x];
+            }
+        }
+
+        var start = root.GetProperty("start");
+        var startX = start.GetProperty("X").GetInt32();
+        var startY = start.GetProperty("Y").GetInt32();
+        var startDirection = start.GetProperty("facing").GetString() switch
+        {
+            "N" => Direction.North,
+            "E" => Direction.East,
+            "S" => Direction.South,
+            "W" => Direction.West,
+            _ => throw new InvalidOperationException("Invalid direction")
+        };
+
+        var commands = root.GetProperty("commands").EnumerateArray()
+            .Select(c => c.GetString() ?? throw new InvalidOperationException("Missing command.")).ToArray();
+        
+        var batteryLevel = root.GetProperty("battery").GetInt32();
+
+        return (room, new(startX, startY), startDirection, commands, batteryLevel);
+    }
+}
---
+++ test/Clero.UnitTests/SerializationTests/InputDeserializerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Clero.Serialization;
+
+namespace Clero.UnitTests.SerializationTests;
+
+public class InputDeserializerTests
+{
+    [Fact]
+    public void InputIsDeserialized()
+    {
+        const string input = """
+                             {
+                               "map": [
+                                 ["S", "S", "S", "S"],
+                                 ["S", "S", "C", "S"],
+                                 ["S", "S", "S", "S"],
+                                 ["S", "null", "S", "S"]
+                               ],
+                               "start": {"X": 3, "Y": 1, "facing": "S"},
+                               "commands": [ "TR","A","C","A","C","TR","A","C"],
+                               "battery": 1094
+                             }
+                             """;
+        
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+        
+        var deserializer = new InputDeserializer();
+
+        var deserialized = deserializer.Deserialize(stream);
+        
+        deserialized.Room.ShouldBe(new[,]
+        {
+            { CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited },
+            { CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.Obstacle, CellKind.DirtyUnvisited },
+            { CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited },
+            { CellKind.DirtyUnvisited, CellKind.Obstacle, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited }
+        });
+
+        deserialized.Position.ShouldBe(new(3, 1));
+        deserialized.Direction.ShouldBe(Direction.South);
+        deserialized.Commands.ShouldBe(new[] { "TR", "A", "C", "A", "C", "TR", "A", "C" });
+        deserialized.BatteryLevel.ShouldBe(1094);
+    }
+    
+    // TODO: Add more tests
+}
