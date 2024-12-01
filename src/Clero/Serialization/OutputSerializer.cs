23f20de69a099cd4c9f1cd5edff2c6ee8a085cdf
Antonín Procházka <antonin@postsharp.net>
2024-12-01 18:13:41 +0100

    Output serializer implemented.

---
+++ src/Clero/Serialization/OutputSerializer.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace Clero.Serialization;
+
+public class OutputSerializer
+{
+    public void Serialize(
+        Stream stream,
+        CellKind[,] map,
+        Position finalPosition,
+        Direction finalDirection,
+        int finalBattery)
+    {
+        IEnumerable<Position> GetVisited()
+        {
+            for (var y = 0; y < map.GetLength(0); y++)
+            {
+                for (var x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] is CellKind.Clean or CellKind.DirtyVisited)
+                    {
+                        yield return new Position(x, y);
+                    }
+                }
+            }
+        }
+        
+        IEnumerable<Position> GetCleaned()
+        {
+            for (var y = 0; y < map.GetLength(0); y++)
+            {
+                for (var x = 0; x < map.GetLength(1); x++)
+                {
+                    if (map[y, x] == CellKind.Clean)
+                    {
+                        yield return new Position(x, y);
+                    }
+                }
+            }
+        }
+        
+        Serialize(stream, GetVisited(), GetCleaned(), finalPosition, finalDirection, finalBattery);
+    }
+    
+    public void Serialize(
+        Stream stream,
+        IEnumerable<Position> visited,
+        IEnumerable<Position> cleaned,
+        Position finalPosition,
+        Direction finalDirection,
+        int finalBattery)
+    {
+        // Using StreamWriter instead of Utf8JsonWriter to keep required formatting.
+        // Not disposing the writer because it would dispose the stream.
+        var writer = new StreamWriter(stream);
+        
+        void WritePositions(string name, IEnumerable<Position> positions)
+        {
+            writer.Write($"\"{name}\" : [");
+            
+            foreach (var position in positions)
+            {
+                writer.Write($"{{ \"X\" : {position.X}, \"Y\" : {position.Y} }}");
+            }
+            
+            writer.Write("]");
+        }
+        
+        writer.WriteLine("{");
+        WritePositions("visited", visited);
+        writer.WriteLine(",");
+        WritePositions("cleaned", cleaned);
+        writer.WriteLine(",");
+        writer.WriteLine(
+            $"\"final\" : {{ \"X\" : {finalPosition.X}, \"Y\" : {finalPosition.Y}, \"facing\" : \"{finalDirection switch
+            {
+                Direction.North => "N",
+                Direction.East => "E",
+                Direction.South => "S",
+                Direction.West => "W",
+                _ => throw new InvalidOperationException($"Invalid direction: {finalDirection}")
+            }}\"}},");
+        writer.WriteLine($"\"battery\" : {finalBattery}");
+        writer.Write("}");
+        
+        writer.Flush();
+    }
+}
---
+++ test/Clero.UnitTests/SerializationTests/OutputSerializerTests.cs
@@ -0,0 +1,44 @@
+﻿using Clero.Serialization;
+using Xunit.Abstractions;
+
+namespace Clero.UnitTests.SerializationTests;
+
+public class OutputSerializerTests : TestBase
+{
+    public OutputSerializerTests(ITestOutputHelper output) : base(output) { }
+
+    [Fact]
+    public void OutputIsSerialized()
+    {
+        var room = new[,]
+        {
+            { CellKind.DirtyUnvisited, CellKind.Clean, CellKind.Clean, CellKind.DirtyVisited },
+            { CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.Obstacle, CellKind.DirtyUnvisited },
+            { CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited },
+            { CellKind.DirtyUnvisited, CellKind.Obstacle, CellKind.DirtyUnvisited, CellKind.DirtyUnvisited }
+        };
+        
+        var finalPosition = new Position(2, 0);
+        var finalDirection = Direction.North;
+        var finalBatteryLevel = 53;
+        
+        using var stream = new MemoryStream();
+        var serializer = new OutputSerializer();
+        serializer.Serialize(stream, room, finalPosition, finalDirection, finalBatteryLevel);
+        
+        stream.Position = 0;
+        using var reader = new StreamReader(stream);
+        var output = reader.ReadToEnd();
+
+        output.ShouldBe("""
+                        {
+                        "visited" : [{ "X" : 1, "Y" : 0 }{ "X" : 2, "Y" : 0 }{ "X" : 3, "Y" : 0 }],
+                        "cleaned" : [{ "X" : 1, "Y" : 0 }{ "X" : 2, "Y" : 0 }],
+                        "final" : { "X" : 2, "Y" : 0, "facing" : "N"},
+                        "battery" : 53
+                        }
+                        """);
+    }
+    
+    // TODO: Add more tests
+}
